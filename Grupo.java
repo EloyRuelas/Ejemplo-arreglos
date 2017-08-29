48e9ac3d39ac0d6045de93adf560fabe66171f2a
Eloy Ruelas <eloy.rue@hotmail.com>
2017-08-29 10:55:48 -0500

    Modificacion para usar la documentacion y metodos auxiliares

--- Grupo.java
+++ Grupo.java
@@ -9,15 +9,45 @@ public class Grupo
         nomMateria=nombreMateria;
     }
     
-    public void inscribir(Estudiante e)
+    /**
+     * Busca un espacio disponible en el arreglo
+     * @return Regresa la primera posicion nula dentro del arreglo
+     */
+    private int buscaEspacioDisponible()
     {
         for(int i=0;i<estudiantes.length;i++)
         {
             if(estudiantes[i]==null)
             {
-                estudiantes[i]= e;
+                return i;
             }
         }
+        return -1;
+    }
+        
+    /**
+     * Inscribe un alumno nuevo en el grupo
+     * @param e Es el objeto estudiante a insribir en el grupo.
+     * @return Regresa verdadero si el estudiante fue inscrito o falso en caso 
+     *          de que no se pudiera inscribir.
+     *          
+     */
+    public boolean inscribir(Estudiante e)
+    {
+        int existe=this.buscarEstudiante(e.dimeClave());
+        if(existe != -1)
+        {
+            return false; //el estudiante ya esta inscrito
+        }
+        
+        int posDisponible=this.buscaEspacioDisponible();
+        if(posDisponible==-1)
+        {
+            return false;   //El arreglo esta lleno
+        }
+        
+        estudiantes[posDisponible]=e;       //inscribe al estudiante
+        return true;        //el estudiante fue inscrito
     }
     
     public void darBaja(int claveEstudiante)
@@ -30,4 +60,22 @@ public class Grupo
             }
         }
     }
+    
+    /**
+     * Busca un estudiante por medio de su clave.
+     * @param claveEstudiante Parametro que representa la clave del estudiante a buscar
+     * @return Regresa la posicion del estudiante en el arreglo o -1 si no existe.
+     */
+    public int buscarEstudiante(int claveEstudiante)
+    {
+        for(int i=0; i< estudiantes.length; i++)
+        {
+            if(estudiantes[i].dimeClave()==claveEstudiante)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
 }
